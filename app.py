5ca747ed284d8c4bc0ce0eaf635ab6275c923501
EC2 Default User <ec2-user@ip-172-31-13-116.eu-west-1.compute.internal>
2025-03-21 23:09:07 +0000

    commit20

--- app.py
+++ app.py
@@ -138,5 +138,5 @@ def predict():
             'success': False
         })
 
-# if __name__ == '__main__':
-#     application.run(host='0.0.0.0', port=8080, debug=True)
+if __name__ == '__main__':
+    application.run(debug=True)
